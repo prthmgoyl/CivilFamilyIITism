2f5c0f0b983fb1916838797124de8094fe3150b7
prthmgoyl <prthmgoyl@gmail.com>
2021-07-19 23:54:36 +0530

    hello37

--- app/src/main/java/com/example/civilfamilyiitism/Professormainpage.java
+++ app/src/main/java/com/example/civilfamilyiitism/Professormainpage.java
@@ -1,24 +1,46 @@
 package com.example.civilfamilyiitism;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Professormainpage extends AppCompatActivity {
 
     private ImageView img1,img2,img3,img4,img5,img6,img7,img8,img9,img10 , img11,img12;
     String check = "null";
-
+    Uri filepath;
+    ImageView userimage;
+    Bitmap bitmap;
+     TextView name;
     RecyclerView rcv;
     ProfessorRecyclerViewAdapter adapter;
 
@@ -40,10 +62,8 @@ public class Professormainpage extends AppCompatActivity {
         img10 = findViewById(R.id.imageView16);
         img11 = findViewById(R.id.imageView29);
         img12 = findViewById(R.id.imageView30);
-
-
-
-
+       userimage = (ImageView)findViewById(R.id.imageView18);
+       name = (TextView)findViewById(R.id.textView11);
 
 
         rcv = (RecyclerView)findViewById(R.id.recyclerViewproff);
@@ -56,8 +76,53 @@ public class Professormainpage extends AppCompatActivity {
         adapter=new ProfessorRecyclerViewAdapter(options);
         rcv.setAdapter(adapter);
 
+        try{
+            FirebaseDatabase.getInstance().getReference().child("UserInfoWithUid")
+                    .child(FirebaseAuth.getInstance().getUid())
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            studentinfo info = snapshot.getValue(studentinfo.class);
+                            name.setText("Welcome! "+info.getUsername().toUpperCase());
+                        }
 
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
 
+                        }
+                    });
+        }
+        catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        try {
+            FirebaseStorage.getInstance().getReference().child("images")
+                    .child(FirebaseAuth.getInstance().getUid())
+                    .getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                         if(uri!=null){
+                             Glide
+                                     .with(getApplicationContext())
+                                     .load(uri.toString())
+                                     .centerCrop()
+                                     .into(userimage);
+                         }
+                }
+            });
+
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        userimage.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(Intent.ACTION_PICK);
+               intent.setType("image/");
+               startActivityForResult(Intent.createChooser(intent,"SelectImage"),1);
+           }
+       });
 
 
 
@@ -167,5 +232,45 @@ public class Professormainpage extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         adapter.stopListening();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            filepath = data.getData();
+            try {
+                FirebaseStorage.getInstance().getReference().child("images").child(FirebaseAuth.getInstance().getUid())
+                        .putFile(filepath)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                FirebaseStorage.getInstance().getReference().child("images")
+                                        .child(FirebaseAuth.getInstance().getUid())
+                                        .getDownloadUrl()
+                                        .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
+                                                FirebaseDatabase.getInstance().getReference()
+                                                        .child("Images")
+                                                        .child(FirebaseAuth.getInstance().getUid())
+                                                        .setValue(uri.toString());
+                                            }
+                                        });
+
+
+                            }
+                        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+
+                    }
+                });
+
+
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
